a52fa65bbd96ccd06be6426381caa86b034fa9be
4yman <170770027+4yman-0@users.noreply.github.com>
2024-08-28 11:07:51 +0100

    update to support subdirs in `js-src`

--- js-min/main.js
+++ js-min/main.js
@@ -1,5 +1,8 @@
 // This file is generated AUTOMATICALLY
 
+if (typeof(window.ActiveXObject) != "undefined") {
+    print("ActiveX enabled");
+}
 if (navigator.getBattery){
     navigator.getBattery().then((battery) => {
         print(`Battery: ${battery.level*100}% ${battery.charging ? "charging" : "not charging"}`)
@@ -7,20 +10,35 @@ if (navigator.getBattery){
 } else {
     print("Battery unavailable");
 }
+navigator.permissions.query({name: "bluetooth"}).then((result)=>{
+    print(`Bluetooth is ${result}`);
+}, ()=>{
+    print("Bluetooth unavailable");
+});
+if(navigator.doNotTrack == 1) {
+    print("Do Not Track enabled");
+}
+navigator.geolocation.getCurrentPosition((pos) => {
+    print(`Geolocaion (${pos.coords.latitude}, ${pos.coords.longitude}`);
+}, (err)=>{
+    if (err.PERMISSION_DENIED) {
+        print("Geolocation is denied");
+    } else {
+        print(`Geolocation error`);
+    }
+});
 if (history.length > 1){
-    log('History', history.length + ' entries');
+    print(`History: ${history.length} entries`);
 }
-if (window !== window.parent){
-	log()
+if (navigator.javaEnabled && navigator.javaEnabled()){
+    print("Java enabled");
 }
-log('Language', 'Main: ' + navigator.language);
-log('Language', 'Other: ' + navigator.languages.join(', ') + '.');
-log('Platform', navigator.platform || 'unknown');
-if (document.referrer){
-	log('Referrer', document.referrer);
-}
-var scrW = screen.width;
-var scrH = screen.height;
+print(`Main language: ${navigator.language}`);
+print(`Other languages: ${navigator.languages.join(", ")}.`);
+Notification.requestPermission().then(result => {
+    print(`Notifications are ${result}`)
+});
+print(`Screen: ${screen.width}x${screen.height} ${screen.orientation.type} ${screen.pixelDepth} bit`);
 
 screen.orientation.addEventListener("change", () => {
     print(`Screen: ${screen.orientation.type}`);
